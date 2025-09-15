e9493449804c82d0470fd09b151f9cc05c615062
LeeDaYeon <57980181+ErigoLee@users.noreply.github.com>
2025-09-15 01:34:17 -0400

    set grabble objects

---
+++ Assets/Scripts/AchivementSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchivementSystem : MonoBehaviour, Observer
+{
+    // OnNotify is called when Achievement System receives a notification from an object
+    public void OnNotify(object obj, NotificationType notificationType)
+    {
+        if (notificationType == NotificationType.GreenCubeCollected)
+        {
+            Debug.Log("Congrats! Achievement unlocked! (Observer Pattern version)");
+        }
+    }
+
+    void Start()
+    {
+        // add Achievement System object as observer to all observable objects
+        foreach (Observable subject in FindObjectsOfType<Observable>())
+        {
+            subject.AddObserver(this);
+        }
+    }
+
+}
+
---
+++ Assets/Scripts/AchivementSystemUsingEvents.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchivementSystemUsingEvents : MonoBehaviour
+{
+
+    void Start()
+    {
+        CubeCollectionNotifier.OnCubeColleted += UnlockCubeCollectedAchievement;
+        
+    }
+
+    private void UnlockCubeCollectedAchievement(CubeCollectionNotifier c)
+    {
+        Debug.Log("Congrats! Achievement unlocked (Event-based Observer)");
+    }
+
+    private void UnlockSphereCollectedAchievement(string c)
+    {
+        Debug.Log(c);
+    }
+
+}
